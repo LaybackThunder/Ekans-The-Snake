a6f0f5aef607500693600319e9c236672d391276
LaybackThunder <glowingblueasteroid@outlook.com>
2023-01-15 20:43:09 -0900

    Created Settings class.

--- client.py
+++ client.py
@@ -1,22 +1,22 @@
 import pygame, sys
-from random import choice, randrange
+from settings import *
 
 class Client():
     """Manage game states and behavior."""
 
     def __init__(self):
         """Initialize states."""
-        self.WINDOW_WIDTH = 400
-        self.WINDOW_HEIGHT = 800
 
-        self.window = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
-        pygame.display.set_caption("Ooooooh!")
+        # Objects instanciate
+        self.settings = Settings()
+
+        # Window comes to life
+        self.window = pygame.display.set_mode(self.settings.WINDOW_DIMENTIONS)
+        pygame.display.set_caption("Ooooooh!") # Headline on top of window's boarders
 
     def run_game(self):
-        """Game begins."""
-
+        """The game begins."""
         while True:
-
             # Event
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -25,6 +25,7 @@ class Client():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_q:
                         self.quit_game()
+                        
             # Draw 
             pygame.display.update()
         
