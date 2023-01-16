56a80a779c349afa8b21a15a69f46b52103887f6
LaybackThunder <glowingblueasteroid@outlook.com>
2023-01-15 20:44:02 -0900

    Created Settings class, refactor code.

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""Place holder for states used by other classes."""
+
+class Settings():
+    
+    def __init__(self):
+        """Initiate attributes."""
+
+        # Window attributes
+        self.WINDOW_WIDTH = 400
+        self.WINDOW_HEIGHT = 800
+        self.WINDOW_DIMENTIONS = (self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
